2d2c5d8570fd3b79b4f30ed2b0e2dd0a98504c29
princeraw <princeraw94@gmail.com>
2025-01-24 14:56:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import pickle
+import pandas as pd
+from flask import Flask , render_template , request
+
+model = pickle.load(open("E:\\Analytix labs\\Machine Learning\\ML 2\\app\\model.pkl",'rb'))
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['GET','POST'])
+def index():
+    if request.method =='GET':
+        return render_template('index.html')
+    else:
+        form_inputs = pd.DataFrame(request.form.to_dict(),index = [0])
+        prediction = model.predict(form_inputs.astype(float))
+        return str(prediction)
+
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
